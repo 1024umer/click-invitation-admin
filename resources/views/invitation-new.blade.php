1bc3a2602b112a819ae695ae6cfe408f4e165026
Rao Talha <talharao997az@gmail.com>
2024-03-07 00:08:26 +0500

    fonts

--- resources/views/invitation-new.blade.php
+++ resources/views/invitation-new.blade.php
@@ -651,7 +651,9 @@ https://cdn.jsdelivr.net/npm/sweetalert2@11.10.5/dist/sweetalert2.min.css
                         <option value="Amoun, sans-serif" style="font-family: 'Amoun', sans-serif;">Amoun</option>
                         <option value="Moon Flower, sans-serif" style="font-family: 'Moon Flower', sans-serif;">Moonflowers</option>
                         <option value="ScarlettBusiat, sans-serif" style="font-family: 'ScarlettBusiat', sans-serif;">ScarlettBusiat</option>
-
+                        <option value="MontserratLight, sans-serif" style="font-family: 'MontserratLight', sans-serif;">Montserrat Light</option>
+                        <option value="BrockScript, sans-serif" style="font-family: 'BrockScript', sans-serif;">BrockScript</option>
+                        
 
                         <option value="jandacelebrationscript, sans-serif" style="font-family: 'jandacelebrationscript', sans-serif;">Janda Celebration Script</option>
                         <option value="anydore, sans-serif" style="font-family: 'anydore', sans-serif;">Anydore</option>
@@ -670,8 +672,7 @@ https://cdn.jsdelivr.net/npm/sweetalert2@11.10.5/dist/sweetalert2.min.css
                         <option value="VanityBoldNarrow, sans-serif" style="font-family: 'VanityBoldNarrow', sans-serif;">VanityBoldNarrow</option>
                         <option value="KugileDemo, sans-serif" style="font-family: 'KugileDemo', sans-serif;">KugileDemo</option>
                         <option value="Atheniademo, sans-serif" style="font-family: 'Atheniademo', sans-serif;">Atheniademo</option>
-                        <option value="MontserratLight, sans-serif" style="font-family: 'MontserratLight', sans-serif;">Montserrat Light</option>
-
+                       
                         
                         <option value="Evilof, sans-serif" style="font-family: 'Evilof', sans-serif;">Evilof</option>
                         <option value="Landliebe, sans-serif" style="font-family: 'Landliebe', sans-serif;">Landliebe
