996c8744a632f0fa50b01a402ee2b920fb4065c2
Muhammad Umer <127780703+1024umer@users.noreply.github.com>
2024-03-25 14:38:13 -0600

    fixes

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -11,9 +11,10 @@ class CardController extends Controller
     public function index()
     {
         $cards = \DB::select('
-        SELECT cu.*, et.*
-        FROM cards_upload cu
-        LEFT JOIN event_type et ON cu.id_eventtype = et.id_eventtype');
+            SELECT cu.*, et.*
+            FROM cards_upload cu
+            LEFT JOIN event_type et ON cu.id_eventtype = et.id_eventtype'
+        );
         $events = EventType::get();
         return view('card')->with(compact('cards', 'events'));
     }
